c38719634a828ffdbd3e8dcbaa7cdf1772a84b3e
Elmir202 <elmireh@code.edu.az>
2023-05-16 08:58:54 +0400

    Tabs task is done

---
+++ main.js
@@ -0,0 +1,34 @@
+var tabs = document.querySelectorAll(".tabs_main ul li");
+var males = document.querySelectorAll(".male");
+var females = document.querySelectorAll(".female");
+var all = document.querySelectorAll(".item_main");
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        tabs.forEach((tab) => {
+            tab.classList.remove("active");
+        })
+        tab.classList.add("active");
+        var tabval=tab.getAttribute("data-tabs")
+
+        all.forEach((item) => {
+            item.style.display = "none";
+        })
+
+        if (tabval == "male") {
+            males.forEach((male) => {
+                male.style.display = "block";
+            })
+        }
+        else if (tabval == "female") {
+            females.forEach((female) => {
+                female.style.display = "block";
+            })
+        }
+        else {
+            all.forEach((item) => {
+                item.style.display = "block";
+            })
+        }
+    })
+})
